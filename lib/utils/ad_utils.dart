c5fa769bda0a1352af4f97f2455e60dd324edb52
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2023-05-02 11:42:38 +0200

    SPOR-55: Ads

--- lib/utils/ad_utils.dart
+++ lib/utils/ad_utils.dart
@@ -15,7 +15,7 @@ class AdUtils {
   }
 
   String get bannerAdId {
-    if (_test) return 'ca-app-pub-3940256099942544/3347511713';
-    return 'ca-app-pub-5475010456455777/8177827519';
+    if (_test) return 'ca-app-pub-3940256099942544/6300978111';
+    return 'ca-app-pub-5475010456455777/2584727667';
   }
 }
